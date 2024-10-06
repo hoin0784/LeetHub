5cd2f5fbe9c345400054687ee1af2125ba89fa88
Hoin Jang <90217198+hoin0784@users.noreply.github.com>
2024-10-05 21:37:24 -0500

    Oct/5 2024 solution

--- Stack/0020-valid-parentheses/0020-valid-parentheses.py
+++ Stack/0020-valid-parentheses/0020-valid-parentheses.py
@@ -16,6 +16,30 @@ class Solution:
                 if not stack or c != parenthese[stack.pop()]:
                     return False
         return not stack 
+
+
+    # Oct 5 2024 solution:
+    
+    class Solution:
+    def isValid(self, s: str) -> bool:
+
+        parentheses = {
+            '(' : ')',
+            '{' : '}',
+            '[' : ']'
+        }
+
+        stack = []
+
+        for c in s:
+            if c in parentheses.keys():
+                stack.append(c)
+            else:
+                if not stack or c != parentheses[stack[-1]]:
+                    return False
+                stack.pop()
+
+        return not stack
         
 
 
